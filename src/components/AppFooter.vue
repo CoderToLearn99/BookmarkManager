6b1b94687e5f2b229fbbb533b2ada9e0ab0f6b34
CoderToLearn99 <kaveripatil2023@gmail.com>
2025-03-03 21:57:02 +0530

    Update AppFooter.vue

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -12,24 +12,6 @@ export default {
 };
 </script>
 
-<style scoped>
-footer {
-  text-align: center;
-  padding: 1rem;
-  background: linear-gradient(135deg, #74ebd5 0%, #9face6 100%);
-  transition: background 0.3s ease;
-}
 
 
-
-footer.light {
-  background: linear-gradient(135deg, #ffffff 0%, #f4f4f4 100%);
-}
-
-/* Dark Mode */
-footer.dark {
-  background-color: #333333;
-  color: #fff;
-}
-</style>
 
